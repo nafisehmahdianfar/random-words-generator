8a351f4b311c8d277ee4099711e7073a298be7b9
nafisehmahdianfar <n.mahdianfar@gmail.com>
2020-12-13 13:22:22 +0330

    setting for create package

---
+++ src/RandomWords.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class RandomWords extends Component {
+    render() {
+        return (
+            <div>
+                <textarea></textarea>
+
+            </div>
+        );
+    }
+}
+
+export default RandomWords;
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+var path = require('path');
+module.exports = {
+    mode: 'production',
+    entry: './src/RandomWords.js',
+    output: {
+        path: path.resolve('lib'),
+        filename: 'RandomWords.js',
+        libraryTarget: 'commonjs2'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules)/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ],
+    },
+    resolve: {
+        alias: {
+            'react': path.resolve(__dirname, './node_modules/react'),
+            'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+        }
+    },
+    externals: {
+        // Don't bundle react or react-dom
+        react: {
+            commonjs: "react",
+            commonjs2: "react",
+            amd: "React",
+            root: "React"
+        },
+        "react-dom": {
+            commonjs: "react-dom",
+            commonjs2: "react-dom",
+            amd: "ReactDOM",
+            root: "ReactDOM"
+        }
+    }
+};
+
+
+
+
+
+
