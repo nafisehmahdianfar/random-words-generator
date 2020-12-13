25b85e8d4cafda9c373ab10b912af28f53228cf0
nafiseh <n.mahdianfar@gmail.com>
2020-12-13 16:11:25 +0330

    get style

--- src/RandomWords.js
+++ src/RandomWords.js
@@ -1,16 +1,43 @@
 import React, {Component} from 'react';
 
 class RandomWords extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            number: 0
+        };
+    }
+
     render() {
         return (
-            <div>
-                <textarea>
-                    <button></button>
-                </textarea>
-
+            <div style={{position: 'absolute', top: '50%', left: '50%'}}>
+                <textarea style={{resize: 'none', margin: '-25px 0px 40px -65px'}}></textarea>
+                <div style={{margin: '-25px 0px 0px -25px'}}>
+                    <button
+                        style={{
+                            color: 'white',
+                            backgroundColor: '#d9534f',
+                            borderRadius: "4px 0px 0px 4px",
+                            fontSize: '20px',
+                            width: '30px'
+                        }}>-
+                    </button>
+                    <input style={{width: '30px', textAlign: 'center', fontSize: '20px'}}
+                    ></input>
+                    <button
+                        style={{
+                            color: 'white',
+                            backgroundColor: '#5cb85c',
+                            borderRadius: "0px 4px 4px 0px",
+                            fontSize: '20px',
+                            width: '30px'
+                        }}>+
+                    </button>
+                </div>
             </div>
         );
     }
+
 }
 
 export default RandomWords;
